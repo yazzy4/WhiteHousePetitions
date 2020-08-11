d07da06be83257da2a1d865eb4ddb18cb7bc8eee
yazzy4 <yazburrell@pursuit.org>
2020-08-11 14:16:09 -0400

    Added detail vc for petition detail in custom html

--- WhiteHousePetitions/View Controllers/ViewController.swift
+++ WhiteHousePetitions/View Controllers/ViewController.swift
@@ -9,30 +9,52 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    
     var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let urlString = "http://www.hackingwithswift.com/samples/petitions-1.json"
-        
-        if let url = URL(string: urlString){
-            if let data = try? Data(contentsOf: url)
-            //good to parse
+
+
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
+            }
         }
+
        
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle text goes here"
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
 }
 
